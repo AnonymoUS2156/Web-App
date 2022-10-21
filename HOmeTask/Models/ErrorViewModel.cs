e9337ac873a456da945903099ce9638a08ded2e8
315 <315@315-15>
2022-10-21 11:27:15 +0300

    Добавьте файлы проекта.

---
+++ HOmeTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HOmeTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
